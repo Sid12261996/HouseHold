0eb2a65075775220fc351689b46b16b5adfd6ac7
shekharsaini98 <37480531+shekharsaini98@users.noreply.github.com>
2019-03-13 02:06:26 +0530

    shekhar : 13/3/19 at 2:06 AM

--- wrapItUp/src/app/material.ts
+++ wrapItUp/src/app/material.ts
@@ -1,12 +1,16 @@
 import {MatButtonModule, MatCheckboxModule, MatDialogModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [MatButtonModule, 
             MatCheckboxModule, 
             MatDialogModule,
-            MatFormFieldModule,    
+            MatFormFieldModule, 
+            MatSidenavModule,
+            MatCardModule 
         ],
 
 
@@ -14,6 +18,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
             MatCheckboxModule,
             MatDialogModule,
             MatFormFieldModule, 
+            MatSidenavModule,
+            MatCardModule
         ],
 })
 export class MaterialModule { }
