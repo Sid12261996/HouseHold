faac7ac271ba4a629e4fc3f486995aad1a69c442
sid12261996 <sidharthrkc@gmail.com>
2019-03-13 17:29:34 +0530

    local changes

--- wrapItUp/src/app/app-routing.module.ts
+++ wrapItUp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { RegisterComponent } from './components/header/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path :'register' ,component: RegisterComponent
+  },
+  { path: '', component: BodyComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- wrapItUp/src/app/components/header/register/register.component.ts
+++ wrapItUp/src/app/components/header/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserService } from 'src/user-service.service';
+import { AppUser } from 'src/app-user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,8 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],  
 })
 export class RegisterComponent implements OnInit {
+  formFields: any;
+  User: AppUser[];
+ constructor(private _loginservice : UserService, private route:Router) { }
   
   ngOnInit() {
+    this.formFields = new FormGroup({
+      Username :new FormControl(),
+      Email :new FormControl(),
+      Password :new FormControl(),
+  
+  ConfirmPassword :new FormControl(),
+      
+      PhoneNumber :new FormControl(),
+      Address :new FormControl(),
+      Country :new FormControl(),
+      State :new FormControl()
+ 
+    });
+
+    
   }
 
+  Register():void{
+    console.log(this.formFields.value);
+    
+     this._loginservice.RegisterUser(this.formFields.value).subscribe( data => {
+       console.log(data);
+       
+       this.route.navigate(['log-in'])
+     });
+}
 }
