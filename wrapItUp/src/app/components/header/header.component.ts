3b7b783236f892abc16e691192a8661cc4812642
shekharsaini98 <37480531+shekharsaini98@users.noreply.github.com>
2019-03-13 17:13:58 +0530

    shekhar : Dialog works 13/3/19 at 5:13 PM

--- wrapItUp/src/app/components/header/header.component.ts
+++ wrapItUp/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { LoginComponent } from './login/login.component';
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeaderComponent implements OnInit {
   
-  constructor() { }
-
   ngOnInit() {
   }
+  constructor(private dialog:MatDialog,
+              private dialog2:MatDialog          
+    ){}
 
+  openDialog(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.width = "300%";
+    this.dialog.open(RegisterComponent)
+  }
+  openDialog2(){
+    const dialogConfig2 = new MatDialogConfig();
+    dialogConfig2.disableClose = true;
+    this.dialog.open(LoginComponent)
+  }
 }
