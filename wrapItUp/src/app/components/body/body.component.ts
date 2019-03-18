19491006c6b881515938a3fcd67843de1ed5be6b
sid12261996 <sidharthrkc@gmail.com>
2019-03-18 12:07:27 +0530

    Sid: getting values of user

--- wrapItUp/src/app/components/body/body.component.ts
+++ wrapItUp/src/app/components/body/body.component.ts
@@ -12,7 +12,7 @@ export class BodyComponent implements OnInit {
   constructor(private dataService:UserService) { }
 user$:AppUser[];
   ngOnInit() {
-    this.dataService.GetUserByEmail('sidharthrkc@gmail.com').subscribe(data=>{this.user$=data;
+    this.dataService.GetUserByEmail('shekharsaini@gmail.com').subscribe(data=>{this.user$=data;
       console.log(data);
     }
       );
