33cf84c858e1f9780f59801f340e8999c3a7aedc
sid12261996 <sidharthrkc@gmail.com>
2019-04-13 22:05:54 +0530

    node added for heroku

---
+++ wrapItUp/server.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+app = express(),
+path = require('path');
+
+app.use(express.static(path.join(__dirname+'\src\dist')));
+console.log(path.join(__dirname+'\src\dist'));
+app.listen(process.env.PORT||5000,()=>console.log('Connected!'));
+
+app.get('/*',(req,res)=>{
+    console.log("route..!")
+    res.sendFile(path.join(__dirname,'/src/dist/index.html'));
+});
