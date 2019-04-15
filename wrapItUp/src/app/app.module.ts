bf4b94ff833c44c54b8469e16cc4d41637173521
sid12261996 <sidharthrkc@gmail.com>
2019-04-15 12:05:15 +0530

    deleted language and font-size components

--- wrapItUp/src/app/app.module.ts
+++ wrapItUp/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LanguageComponent } from './components/header/language/language.component';
-import { FontSizeComponent } from './components/header/font-size/font-size.component';
+
 import { RegisterComponent } from './components/header/register/register.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
@@ -25,8 +24,7 @@ import { SecurityService } from './Security/security.service';
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    LanguageComponent,
-    FontSizeComponent,
+   
     RegisterComponent,
     BodyComponent,
     LoginComponent,
