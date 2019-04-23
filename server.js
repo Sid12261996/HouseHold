186bcd059b5ff92c749bd902aca15e282e676415
sid12261996 <sidharthrkc@gmail.com>
2019-04-23 11:53:46 +0530

    server config

--- server.js
+++ server.js
@@ -2,25 +2,11 @@ const express = require('express'),
 app = express(),
 path = require('path');
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://wraapitup.herokuapp.com/api/getall");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, contenttype, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-  
-    next();
-  });
-
-app.use(express.static(path.join(__dirname+'/dist/wrapItUp')));
-console.log(path.join(__dirname+'/dist/wrapItUp'));
+app.use(express.static(path.join(__dirname+'/wrapItUp/dist/wrapItUp')));
+console.log(path.join(__dirname+'/wrapItUp/dist/wrapItUp'));
 app.listen(process.env.PORT||5000,()=>console.log('Connected!'));
 
 app.get('/*',(req,res)=>{
     console.log("route..!")
-    res.sendFile(path.join(__dirname,'/dist/wrapItUp/index.html'));
+    res.sendFile(path.join(__dirname,'/wrapItUp/dist/wrapItUp/index.html'));
 });
