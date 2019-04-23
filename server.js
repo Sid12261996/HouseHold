02bf88c736444d26a0537793b0c4c718c2ce64a2
sid12261996 <sidharthrkc@gmail.com>
2019-04-23 17:09:48 +0530

    Config to heroku

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express'),
 app = express(),
 path = require('path');
 
-app.use(express.static(path.join(__dirname+'/wrapItUp/dist/wrapItUp')));
-console.log(path.join(__dirname+'/wrapItUp/dist/wrapItUp'));
+app.use(express.static(path.join(__dirname+'/dist/wrapItUp')));
+console.log(path.join(__dirname+'/dist/wrapItUp'));
 app.listen(process.env.PORT||5000,()=>console.log('Connected!'));
 
 app.get('/*',(req,res)=>{
     console.log("route..!")
-    res.sendFile(path.join(__dirname,'/wrapItUp/dist/wrapItUp/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/wrapItUp/index.html'));
 });
