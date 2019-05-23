76c28afd7ae5c3106ab3e95348297a3e7a73a084
sid12261996 <sidharthrkc@gmail.com>
2019-05-23 16:01:07 +0530

    Latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {MaterialModule} from './models/material';
 import {LoginComponent} from './components/header/login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {UserService} from 'src/app/services/user-service.service';
-import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {HttpClientModule, HttpClient, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {IndexComponent} from './components/index/index.component';
 import {WelcomepageComponent} from './components/body/welcomepage/welcomepage.component';
 import {PopUpService} from './services/pop-up.service';
@@ -21,6 +21,8 @@ import {CountryService} from './services/country.service';
 import {SecurityService} from './services/security.service';
 import {AuthGuardGuard} from './Security/auth-guard.guard';
 import {Router} from '@angular/router';
+import { ProfileComponent } from './components/body/profile/profile.component';
+import {JwtInterceptor} from './myInterceptor';
 
 @NgModule({
   declarations: [
@@ -33,6 +35,7 @@ import {Router} from '@angular/router';
     LoginComponent,
     IndexComponent,
     WelcomepageComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,7 @@ import {Router} from '@angular/router';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [ UserService, HttpClientModule, HttpClient, PopUpService, CountryService, SecurityService, AuthGuardGuard],
+  providers: [ UserService, HttpClientModule, HttpClient, PopUpService, CountryService, SecurityService, AuthGuardGuard,{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }],
   bootstrap: [AppComponent],
   entryComponents: [AppComponent]
 })
