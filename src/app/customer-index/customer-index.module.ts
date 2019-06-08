862dab52fd1c0a207cea4bf15031bd54b8a36c34
sid12261996 <sidharthrkc@gmail.com>
2019-06-09 00:05:51 +0530

    service-history and status

--- src/app/customer-index/customer-index.module.ts
+++ src/app/customer-index/customer-index.module.ts
@@ -5,16 +5,21 @@ import {CustomerIndexRoutingModule} from './customer-index-routing.module';
 import {SubCategoryComponent} from './sub-category/sub-category.component';
 import {MaterialModule} from '../models/material';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {ReschedulePopupComponent} from './IndexModule/status/reschedule-popup/reschedule-popup.component';
+import {FormsModule} from "@angular/forms";
+import {MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
-  declarations: [SubCategoryComponent],
+  declarations: [SubCategoryComponent, ReschedulePopupComponent],
   imports: [
     CommonModule,
-    CustomerIndexRoutingModule, MaterialModule
+    CustomerIndexRoutingModule, MaterialModule, FormsModule, MatDatepickerModule, MatNativeDateModule
   ],
-  exports: [CustomerIndexRoutingModule],
+  exports: [CustomerIndexRoutingModule, ReschedulePopupComponent],
+  entryComponents:[ReschedulePopupComponent],
   providers: [MatSnackBar]
 })
 export class CustomerIndexModule {
+
 }
