bbb9686724d47bc37bfc31d87456f002e0e899ef
sid12261996 <sidharthrkc@gmail.com>
2019-06-11 06:52:10 +0530

    cors added

--- server.js
+++ server.js
@@ -4,19 +4,7 @@ path = require('path'),
 cors= require('cors');
 
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://wraapitup.herokuapp.com/api/getall");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, contenttype, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-  
-    next();
-  });
+
 
 app.use(express.static(path.join(__dirname+'/dist/wrapItUp')));
 console.log(path.join(__dirname+'/dist/wrapItUp'));
